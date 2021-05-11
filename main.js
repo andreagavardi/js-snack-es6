3f7de313cf89cddcc533538f4413caf0ad690c9f
andreagavardi <andreagavardi89@gmail.com>
2021-05-11 16:20:46 +0200

    terminato snack 2

--- main.js
+++ main.js
@@ -59,9 +59,47 @@ document.getElementById("bicicletta").innerHTML = `
 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà:
 nome,
 punti fatti,
-falli subiti.
-Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
-Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
- */
+falli subiti.*/
+//Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+
+const squadre = [
+    {
+        nome: 'Inter',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Milan',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Juventus',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Lazio',
+        punti: 0,
+        falli: 0
+    },
+];
+
+function randomNumbers(){
+    return Math.floor(Math.random()*50);
+}
+
+
+const squadreCopia = [];
+for(let i = 0; i < squadre.length; i++){
+    let squadraEl = squadre[i];         
+    squadraEl.punti = randomNumbers(); //sostituisco i valori di falli e punti con numeri casuali
+    squadraEl.falli = randomNumbers();//Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+    const { nome, falli} = squadraEl //Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+    squadreCopia.push({ nome, falli});
+}
+console.log(squadre);
+console.log(squadreCopia);
+
+
 
